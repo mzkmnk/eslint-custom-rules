00e6de24fb827959b9b39446aa8c600cf060dc83
mizuki <114119396+mzkmnk@users.noreply.github.com>
2025-02-23 20:38:13 +0900

    refactor: add semantic release (#2)

---
+++ release.config.js
@@ -0,0 +1,20 @@
+export default {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+
+    "@semantic-release/release-notes-generator",
+
+    '@semantic-release/changelog',
+
+    '@semantic-release/npm',
+
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'package-lock.json'],
+      },
+    ],
+
+    '@semantic-release/github',
+  ],
+};
